5f99177f0eceb1cc23071a61c5cbc1fdcc60b0b5
Aashish Upmanyu <aupmanyu@deloitte.co.uk>
2018-07-21 23:46:33 +0100

    Add express server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * index.js
- */
-const logSomething = options => ({
-  ...options,
-  anotherOption: 'Hello!',
+import http from 'http';
+import express from 'express';
+// Express app setup
+const app = express();
+
+const server = http.createServer(app);
+server.listen(3000);
+server.on('listening', () => {
+  console.log('Server is listening on port: 3000');
 });
-const options = logSomething({ one: '1', two: '2' });
-console.log(options);
+
+app.get('*', (req, res) => {
+  res.end('Hello Express');
+});
